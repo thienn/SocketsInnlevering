796821e6e2f8467891da0fe85e45775a40002178
thienn <thien.oslo@gmail.com>
2017-11-12 00:33:11 +0100

    Init upload

---
+++ SocketsPGR200Innlevering/src/main/java/no/westerdals/Client.java
@@ -0,0 +1,32 @@
+package no.westerdals;
+
+import java.io.IOException;
+
+public class Client {
+    public static void main(String[] args) {
+        DBHandler program = new DBHandler();
+
+        try {
+            program.getConnection();
+            System.out.println("Database Connection Success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Drops table if it exist - for testing purposes
+        program.dropTable();
+
+        // Create table
+        program.createTable();
+
+        // Runs through method for reading from CSV File
+        try {
+            program.readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Setup of DB and populating successful");
+        // Ask for user input - Read part
+        program.userInput();
+    }
+}
